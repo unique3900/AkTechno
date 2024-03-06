8bf2bc2a945e24202e6f67c18b27e14fe93464cb
unique3900 <parasharneupane3900@gmail.com>
2024-03-06 13:46:10 +0545

    Next app created

---
+++ client/src/app/page.js
@@ -0,0 +1,10 @@
+
+const page = () => {
+  return (
+    <div className="min-h-screen  bg-black">
+      <h1 className="text-center font-extrabold text-emerald-500">AK Techno</h1>
+      </div>
+  )
+}
+
+export default page
